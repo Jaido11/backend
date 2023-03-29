6c46c3f9099b8279fca912449e690df2995b1e00
someone <someone@someplace.com>
2023-03-29 23:57:29 +0800

    Added Show functionality in CarouselItemsController

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/carousel', [CarouselItemsController::class, 'index']);
+Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
